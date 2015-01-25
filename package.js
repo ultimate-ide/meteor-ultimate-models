b8f21393f61f9b3d204c8840df442794268376a8
James Gillmore <james@faceyspacey.com>
2015-01-24 16:52:24 -0800

    total revamp

--- package.js
+++ package.js
@@ -10,8 +10,18 @@ Package.on_use(function (api, where) {
   api.use(["underscore"]);
 
   api.add_files([
-    'lib/base_model.js',
-    'lib/model_factory.js',
+    'lib/inheritance.js',
+    'lib/base_class.js',
+		
+    'lib/extend_http/base_http.js',
+    'lib/extend_http/meteor_method.js',
+    'lib/extend_http/stub_obj.js',
+    'lib/extend_http/meteor_call.js',
+    'lib/extend_http/startup.js',
+		
+		'lib/static_methods.js',
+		
+		'lib/base_model.js',
   ], ['client', 'server']);
   api.export && api.export(['Model'], ['client', 'server']);
 });

0204497da0750f4b53fe46d7383a4d0e2323879b
James Gillmore <james@faceyspacey.com>
2015-02-26 11:26:55 -0800

    complete refactoring

--- package.js
+++ package.js
@@ -1,68 +1,131 @@
 Package.describe({
-  summary: "MVC-like models in meteor",
-  version: "0.0.8",
-  git: "https://github.com/nucleuside/meteor-smart-models.git",
-  name: "nucleuside:smart-models"
+  summary: "MVC-like models in Meteor & A Lot More",
+  version: "0.0.1",
+  git: "https://github.com/ultimate-ide/ultimate-models.git",
+  name: "ultimateide:ultimate-models"
 });
 
 Package.on_use(function (api, where) {
-  api.versionsFrom("METEOR@0.9.1");
+  api.versionsFrom('METEOR@1.0');
   api.use([
 		"underscore",
     'templating',
     'blaze',
+		'http',
 		'aldeed:simple-schema',
 		'aldeed:autoform'
 	]);
 
+
   api.add_files([
-    'lib/inheritance.js',
+    'lib/core/extend/__extend/setup.js',
+		'lib/core/extend/__extend/setup_hook.js',
+		'lib/core/extend/__extend/__extend.js',
+		'lib/core/extend/extend.js',
+		'lib/core/extend/mixin.js',
+		'lib/core/extend/utility.js',
+		'lib/core/inheritance/helper.js',
+		'lib/core/inheritance/extends.js',
+
 		
-    'lib/extend_http/base_http.js',
-    'lib/extend_http/meteor_method.js',
-    'lib/extend_http/stub_obj.js',
-    'lib/extend_http/meteor_call.js',
-    'lib/extend_http/startup.js',	
+		'lib/core_http/ultimate_http.js',
+		'lib/core_http/meteor_method.js',
+		'lib/core_http/stub_obj.js',
+		'lib/core_http/meteor_call.js',
+		'lib/core_http/startup.js',
 		
-		'lib/static_methods.js',
-    'lib/base_class.js',
 		
-		'lib/base_model.js',
+		'lib/core/ultimate_class/ultimate_class.js',
+		'lib/core/ultimate_class/tracker.js',
+		'lib/core/ultimate_class/timer.js',
+		'lib/core/ultimate_class/http.js',
+		'lib/core/ultimate_class/sync.js',	
+		
   ], ['client', 'server']);
+
+  api.export(['UltimateClass'], ['client', 'server']);
+	
+	
+	
+	
+	/** ULTIMATE_FORM & ULTIMATE_MODEL **/
+	
+	api.use([
+		'smeevil:session-store'
+	]);
 	
 	api.add_files([
-		'lib/ultimate_sync.js'
+		'lib/form/ultimate_form.js',
+
+		'lib/model/ultimate_model.js',
+		'lib/model/additional_methods.js',
+		'lib/model/reactive_methods.js',
+		'lib/model/model_extends.js',
+
+  ], ['client', 'server']);
+
+	api.export(['UltimateForm', 'UltimateModel'], ['client', 'server']);
+
+
+	api.add_files([
+		'lib/form/templates.html',
+		'lib/form/templates.js',
+	], ['client']);
+		
+		
+		
+		
+	/** ULTIMATE_SYNC **/
+	
+	api.add_files([
+		'lib/utilities/ultimate_sync.js',
 	], ['server']);
 
+	api.export(['UltimateSync'], ['server']);
 	
 	
-  api.export && api.export(['Base', 'Model', 'BaseHTTP'], ['client', 'server']);
-	api.export && api.export(['UltimateSync'], ['server']);
 	
 	
 	/** UI TOOLS **/
+	
 	api.use([
 	  'tracker',
 	  'session',
 		'naxio:flash'
 	], 'client');
 
-	api.use('aldeed:autoform@3.0.0 || 4.0.0', 'client');
 
 	api.addFiles([
-	  'lib/wizard.html',
-	  'lib/wizard.js',
-		'lib/templates.js',
-	
-		'lib/modal/modal.js',
-		'lib/modal/modal_content.js',
-		'lib/modal/modal_wizard.js',
-		'lib/modal/modal_tabbed.js',
-		'lib/modal/modal_form.js',
-		'lib/modal/model_prompt.js',
-		'lib/modal/schema_prompt.js',
-		'lib/modal/ultimate_prompt.js'
+		'lib/ui/modal/core/ultimate_modal.js',
+		'lib/ui/modal/core/ultimate_modal_form.js',
+		'lib/ui/modal/popups/ultimate_modal_content.js',
+		
+		
+		'lib/ui/modal/popups/ultimate_model_prompt.js',
+		'lib/ui/modal/popups/ultimate_schema_prompt.js',
+		'lib/ui/modal/core/ultimate_prompt.js',
+		
+		'lib/ui/modal/popups/ultimate_modal_wizard.js',
+		'lib/ui/modal/popups/ultimate_modal_tabbed.js',
+		
+	  'lib/ui/modal/templates.html',
+		'lib/ui/modal/templates.js',
+
+
+	  'lib/ui/wizard/ultimate_wizard.js',
+		'lib/ui/wizard/templates.html',
+	  'lib/ui/wizard/templates.js',
+
 	], 'client');
 
-	api.export(['wizardsById', 'wiz'], ['client']);
+	api.export([ 
+		'UltimateModal', 
+		'UltimateModalForm', 
+		'UltimatePrompt',
+		'UltimateModalContent', 
+		'UltimateModalTabbed', 
+		'UltimateModalWizard',
+		'UltimateWizards',
+		'UltimateWizard'
+	], ['client']);
 });
